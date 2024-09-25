fcf7c7152435f6d90b66f9afb62304421d94d745
Ayush-24-mishra <118288016+Ayush-24-mishra@users.noreply.github.com>
2024-09-25 16:13:09 +0530

    program2.py

--- program2.py
+++ program2.py
@@ -4,6 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        values = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    
+    symbols = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    
+    roman_string = ""
+    index = 0
+    while number > 0:
+        while number >= values[index]:
+            roman_string += symbols[index]
+            number -= values[index]
+        index += 1
+    return roman_string
         pass
 
 
