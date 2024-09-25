034a91bdc6a474a0cc9f46095d98f04a02841f94
Ayush-24-mishra <118288016+Ayush-24-mishra@users.noreply.github.com>
2024-09-25 16:11:16 +0530

    program1.py

--- program1.py
+++ program1.py
@@ -4,6 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        parentheses_map = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in parentheses_map.values():  # Check for opening brackets
+            stack.append(char)
+        elif char in parentheses_map.keys():  # Check for closing brackets
+            if not stack or stack.pop() != parentheses_map[char]:
+                return False  # Mismatched or unbalanced brackets
+
+    return len(stack) == 0 
         pass
 
 
